5c6b6458c658524fa67bc2dd0e2d532607dcf304
zkb034 <103853014+zkb034@users.noreply.github.com>
2023-06-11 18:49:00 +0500

    Create AboutStyles.js

---
+++ src/pages/AboutStyles.js
@@ -0,0 +1,41 @@
+const aboutStyles = {
+    ard: {
+        boxShadow: "0px 4px 12px rgba(0, 0, 0, 0.25)",
+        marginBottom: "20px",
+        padding: "20px",
+        cursor: "pointer",
+        transition: "background 0.3s ease", // Add transition for background color change
+      },
+      cardBackground: {
+        light: {
+          background: "#FFFFFF",
+          color: "#000000",
+        },
+        dark: {
+          background: "#000000",
+          color: "#FFFFFF",
+        },
+      },
+  teamMembers: [
+    {
+      name: "Zohair Khan",
+      role: "Software Engineer",
+      github: "https://github.com/zohairalamkhan",
+      linkedin: "https://www.linkedin.com/in/zohair-khan",
+    },
+    {
+      name: "Muhammad Talha",
+      role: "Frontend Developer",
+      github: "https://github.com/talhaabbasi",
+      linkedin: "https://www.linkedin.com/in/muhammad-talha-abbasi",
+    },
+    {
+      name: "Laiba Zulfiqar",
+      role: "UI/UX Designer",
+      github: "https://github.com/laibazulfiqar",
+      linkedin: "https://www.linkedin.com/in/laiba-zulfiqar",
+    },
+  ],
+};
+
+export default aboutStyles;
