65fe8ee2361ee4b872114a43c8fced031f052792
zkb034 <103853014+zkb034@users.noreply.github.com>
2023-06-11 16:10:07 +0500

    Update Hero.js

--- src/pages/Hero.js
+++ src/pages/Hero.js
@@ -5,7 +5,7 @@ import {
   FaBitcoin,
   FaLink,
   FaRainbow,
-  FaCircle
+  FaCircle,
 } from "react-icons/fa";
 import { CSSTransition } from "react-transition-group";
 
@@ -35,139 +35,88 @@ function HeroHome() {
   };
 
   const continueAsGuest = () => {
-    localStorage.setItem("walletAddress", "0x0000000000000000000000000000000000000000");
+    localStorage.setItem(
+      "walletAddress",
+      "0x0000000000000000000000000000000000000000"
+    );
     navigate("/app");
   };
 
   return (
-    <>
-      <section
-        className="w-full dark relative bg-black flex flex-col h-screen justify-center items-center md:flex-row"
-        style={{
-          overflow: "hidden",
-          paddingTop: "8rem",
-          paddingBottom: "16rem",
-        }}
-      >
-        {/* Illustration behind hero content */}
-        <div
-          className="absolute animate-float transform -translate-x-1/2 pointer-events-none"
-          style={{ top: "50%", left: "50%", transform: "translate(-50%, -50%)" }}
+    <section className="w-full dark relative bg-black flex flex-col h-screen justify-center items-center md:flex-row overflow-hidden">
+      {/* Illustration behind hero content */}
+      <div className="absolute left-0 right-0 top-0 bottom-0">
+        <svg
+          width="1360"
+          height="578"
+          viewBox="0 0 1360 578"
+          xmlns="http://www.w3.org/2000/svg"
         >
-          <svg
-            width="1360"
-            height="578"
-            viewBox="0 0 1360 578"
-            xmlns="http://www.w3.org/2000/svg"
-            style={{ width: "100%", height: "auto" }}
-          >
-            <defs>
-              <linearGradient
-                x1="50%"
-                y1="0%"
-                x2="50%"
-                y2="100%"
-                id="illustration-01"
-              >
-                <stop stopColor="#3B84F5" offset="0%" />
-                <stop stopColor="#34A9DC" offset="77.402%" />
-                <stop stopColor="#2DD3BF" offset="100%" />
-              </linearGradient>
-            </defs>
-            <g fill="url(#illustration-01)" fillRule="evenodd">
-              <circle className="left-20" cx="1232" cy="128" r="128" />
-              <circle cx="155" cy="443" r="64" />
-            </g>
-          </svg>
-        </div>
+          <defs>
+            <linearGradient
+              x1="50%"
+              y1="0%"
+              x2="50%"
+              y2="100%"
+              id="illustration-01"
+            >
+              <stop stopColor="#3B84F5" offset="0%" />
+              <stop stopColor="#34A9DC" offset="77.402%" />
+              <stop stopColor="#2DD3BF" offset="100%" />
+            </linearGradient>
+          </defs>
+          <g fill="url(#illustration-01)" fillRule="evenodd">
+            <circle cx="1232" cy="128" r="128" />
+            <circle cx="155" cy="443" r="64" />
+          </g>
+        </svg>
+      </div>
 
-        <div className="max-w-7xl mx-auto px-4 sm:px-6">
-          {/* Hero content */}
-          <div
-            className="pt-8 pb-16 md:pt-20 md:pb-32"
-            style={{
-              position: "relative",
-              zIndex: 1,
-              textAlign: "center",
-              maxWidth: "42rem",
-              margin: "0 auto",
-            }}
-          >
-            {/* Section header */}
-            <div className="text-center pb-8 md:pb-12 section-content">
-              <h1
-                className="text-4xl md:text-5xl text-white md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4"
+      <div className="max-w-7xl mx-auto px-4 sm:px-6">
+        {/* Hero content */}
+        <div className="pt-8 pb-16 md:pt-20 md:pb-32">
+          {/* Section header */}
+          <div className="text-center pb-8 md:pb-12 section-content">
+            <h1
+              className="text-4xl md:text-5xl text-white md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4"
+              data-aos="zoom-y-out"
+            >
+              Filia
+            </h1>
+            <div className="max-w-3xl mx-auto">
+              <p
+                className="text-sm md:text-lg text-gray-400 mb-6 md:mb-8"
                 data-aos="zoom-y-out"
-                style={{
-                  fontSize: "2.5rem",
-                  lineHeight: "1.1",
-                  fontWeight: "800",
-                  letterSpacing: "-0.025em",
-                  marginTop: 0,
-                  marginBottom: "1rem",
-                }}
+                data-aos-delay="150"
               >
-                Filia
-              </h1>
-              <div className="max-w-3xl mx-auto">
-                <p
-                  className="text-sm md:text-lg text-gray-400 mb-6 md:mb-8"
-                  data-aos="zoom-y-out"
-                  data-aos-delay="150"
-                  style={{
-                    fontSize: "0.875rem",
-                    lineHeight: "1.6",
-                    marginTop: 0,
-                    marginBottom: "1rem",
-                    color: "#a1a1aa",
-                  }}
+                Filia is built on top of the Ethereum network, allowing users
+                to create, share, and watch videos without worrying about their
+                privacy.
+              </p>
+              {/* Connect wallet button */}
+              <div className="flex justify-center">
+                <button
+                  className="flex items-center bg-white rounded-full font-medium p-3 md:p-4 shadow-lg wallet-button"
+                  onClick={() => setShowWalletOptions(true)}
                 >
-                  Filia is built on top of the Ethereum network, allowing users
-                  to create, share, and watch videos without worrying about
-                  their privacy.
-                </p>
-                {/* Connect wallet button */}
-                <div className="flex justify-center">
-                  <button
-                    className="flex items-center bg-white rounded-full font-medium p-3 md:p-4 shadow-lg wallet-button"
-                    onClick={() => setShowWalletOptions(true)}
-                    style={{
-                      fontSize: "1rem",
-                      lineHeight: "1.25",
-                      fontWeight: "500",
-                      padding: "0.75rem 1rem",
-                      marginTop: 0,
-                      marginBottom: "1rem",
-                      boxShadow:
-                        "rgba(0, 0, 0, 0.16) 0px 1px 4px, rgba(0, 0, 0, 0.12) 0px 2px 16px",
-                    }}
-                  >
-                    <FaWallet className="mr-2" size={18} />
-                    <span className="text-sm md:text-base">
-                      Connect your wallet to continue
-                    </span>
-                  </button>
-                </div>
-                {/* Continue as guest link */}
-                <p
-                  onClick={continueAsGuest}
-                  className="text-gray-400 mt-4 md:mt-5 guest-link text-center text-xs md:text-sm"
-                  style={{
-                    fontSize: "0.75rem",
-                    lineHeight: "1.4",
-                    marginTop: "1rem",
-                    marginBottom: 0,
-                    cursor: "pointer",
-                    color: "#a1a1aa",
-                  }}
-                >
-                  Continue as a guest
-                </p>
+                  <FaWallet className="mr-2" size={18} />
+                  <span className="text-sm md:text-base">
+                    Connect your wallet to continue
+                  </span>
+                </button>
               </div>
+              {/* Continue as guest link */}
+              <p
+                onClick={continueAsGuest}
+                className="text-gray-400 mt-4 md:mt-5 guest-link text-center text-xs md:text-sm"
+              >
+                Continue as a guest
+              </p>
             </div>
           </div>
         </div>
-      </section>
+      </div>
+
       {/* Wallet options pop-up modal */}
       <CSSTransition
         in={showWalletOptions}
@@ -175,10 +124,7 @@ function HeroHome() {
         classNames="wallet-options"
         unmountOnExit
       >
-        <div
-          className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-75"
-          style={{ zIndex: 999 }}
-        >
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-75">
           <div className="bg-white p-6 rounded-lg">
             <h2 className="text-2xl font-bold mb-4 text-center">
               Select a Wallet
@@ -198,7 +144,22 @@ function HeroHome() {
               Coinbase
             </button>
             <button
-              className="block w-full py-2 px-4 rounded bg-blue-500 text-white font-semibold"
+              className="block w-full py-2 px-4 mb-4 rounded bg-blue-500 text-white font-semibold flex items-center"
+              onClick={() => connectWallet("wallet3")}
+            >
+              <FaCircle className="mr-2" size={16} />
+              WalletConnect
+            </button>
+            <button
+              className="block w-full py-2 px-4 mb-4 rounded bg-blue-500 text-white font-semibold flex items-center"
+              onClick={() => connectWallet("wallet4")}
+            >
+              <FaRainbow className="mr-2" size={16} />
+              Rainbow
+            </button>
+            {/* Add more wallet options as needed */}
+            <button
+              className="block w-full py-2 px-4 rounded bg-gray-300 font-semibold"
               onClick={() => setShowWalletOptions(false)}
             >
               Cancel
@@ -206,7 +167,7 @@ function HeroHome() {
           </div>
         </div>
       </CSSTransition>
-    </>
+    </section>
   );
 }
 
