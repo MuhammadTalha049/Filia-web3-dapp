ee1c7468965f9f6336dc54d821c1cfd7a844d1fe
zkb034 <103853014+zkb034@users.noreply.github.com>
2023-06-11 18:51:50 +0500

    Update Voting.js

--- src/pages/Voting.js
+++ src/pages/Voting.js
@@ -4,8 +4,6 @@ import { IoIosArrowUp } from "react-icons/io";
 import { BsFillPersonFill } from "react-icons/bs";
 
 import { ThemeContext } from "../utils/ThemeContext";
-import { FiGithub, FiLinkedin } from "react-icons/fi";
-import aboutStyles from "./AboutStyles";
 import { Header } from "../components/Header";
 import { Footer } from "../components/Footer";
 import Sidebar from "../components/Sidebar";
@@ -18,7 +16,45 @@ const VotingPage = () => {
   ]);
 
   const [votedAddresses, setVotedAddresses] = useState([]);
-
+  const aboutStyles = {
+    ard: {
+        boxShadow: "0px 4px 12px rgba(0, 0, 0, 0.25)",
+        marginBottom: "20px",
+        padding: "20px",
+        cursor: "pointer",
+        transition: "background 0.3s ease", // Add transition for background color change
+      },
+      cardBackground: {
+        light: {
+          background: "#FFFFFF",
+          color: "#000000",
+        },
+        dark: {
+          background: "#000000",
+          color: "#FFFFFF",
+        },
+      },
+  teamMembers: [
+    {
+      name: "Zohair Khan",
+      role: "Software Engineer",
+      github: "https://github.com/zohairalamkhan",
+      linkedin: "https://www.linkedin.com/in/zohair-khan",
+    },
+    {
+      name: "Muhammad Talha",
+      role: "Frontend Developer",
+      github: "https://github.com/talhaabbasi",
+      linkedin: "https://www.linkedin.com/in/muhammad-talha-abbasi",
+    },
+    {
+      name: "Laiba Zulfiqar",
+      role: "UI/UX Designer",
+      github: "https://github.com/laibazulfiqar",
+      linkedin: "https://www.linkedin.com/in/laiba-zulfiqar",
+    },
+  ],
+};
   useEffect(() => {
     const storedLeaders = localStorage.getItem("leaders");
     const storedVotedAddresses = localStorage.getItem("votedAddresses");
