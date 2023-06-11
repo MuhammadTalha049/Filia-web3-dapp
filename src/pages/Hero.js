b06806846ee353511b75ff2dc53fb6cd3dd1c85c
zkb034 <103853014+zkb034@users.noreply.github.com>
2023-06-11 16:15:18 +0500

    Update Hero.js

--- src/pages/Hero.js
+++ src/pages/Hero.js
@@ -49,32 +49,7 @@ function HeroHome() {
       style={{ height: "100vh", width: "100vw" }}
     >
       {/* Illustration behind hero content */}
-      <div className="absolute left-0 right-0 top-0 bottom-0">
-        <svg
-          width="1360"
-          height="578"
-          viewBox="0 0 1360 578"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <defs>
-            <linearGradient
-              x1="50%"
-              y1="0%"
-              x2="50%"
-              y2="100%"
-              id="illustration-01"
-            >
-              <stop stopColor="#3B84F5" offset="0%" />
-              <stop stopColor="#34A9DC" offset="77.402%" />
-              <stop stopColor="#2DD3BF" offset="100%" />
-            </linearGradient>
-          </defs>
-          <g fill="url(#illustration-01)" fillRule="evenodd">
-            <circle cx="1232" cy="128" r="128" />
-            <circle cx="155" cy="443" r="64" />
-          </g>
-        </svg>
-      </div>
+     
 
       <div className="max-w-7xl mx-auto px-4 sm:px-6">
         {/* Hero content */}
